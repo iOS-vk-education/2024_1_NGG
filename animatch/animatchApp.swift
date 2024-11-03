dbbef29ed5c6124b648fde1f6ed710f9b912a54d
sechensy <90211409+sechensy@users.noreply.github.com>
2024-11-03 21:42:27 +0300

    добавила пустой проект

---
+++ animatch/animatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  animatchApp.swift
+//  animatch
+//
+//  Created by Ксения Панкратова on 03.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct animatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
