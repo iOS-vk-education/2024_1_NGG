d2d90510af49f48cf1fe9fdfcd592df8a2fdc2da
Дмитрий Пермяков <permyakov.d5@wb.ru>
2024-11-21 22:44:07 +0300

    IOS-11: Рефакторинг EditProfileView

---
+++ animatch/CommonViews/NGGTextField.swift
@@ -0,0 +1,42 @@
+//
+//  NGGTextField.swift
+//  animatch
+//
+//  Created by Ксения Панкратова on 18.11.2024.
+//
+
+import SwiftUI
+
+struct NGGTextField: View {
+    let title: String
+    @Binding var text: String
+    @FocusState private var isTyping: Bool
+
+    var body: some View {
+        ZStack(alignment: .leading) {
+
+            TextField("", text: $text)
+                .padding(.leading, 16)
+                .frame(width: 272, height: 51)
+                .foregroundColor(.white)
+                .background(RoundedRectangle(cornerRadius: 8).fill(Color.editProfGray))
+                .focused($isTyping)
+
+            if text.isEmpty {
+                Text(title)
+                    .padding(.horizontal, 16)
+                    .foregroundColor(Color.editProfGrayLight)
+            }
+        }.padding(.bottom, 30)
+    }
+}
+
+// MARK: - Preview
+
+#Preview("Text") {
+    NGGTextField(title: "Title", text: .constant("Input Text"))
+}
+
+#Preview("Placeholder") {
+    NGGTextField(title: "Title", text: .constant(""))
+}
---
+++ animatch/EditProfileView/EditProfileView.swift
@@ -0,0 +1,142 @@
+//
+//  EditProfileView.swift
+//  animatch
+//
+//  Created by Ксения Панкратова on 18.11.2024.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    @State var inputName = ""
+    @State var inputSurname = ""
+    @State var inputEmail = ""
+
+    var body: some View {
+        VStack {
+            ZStack {
+                backgroundLineGradient
+
+                VStack(spacing: 0) {
+                    headerView
+                    Spacer()
+                    formsContainer
+                    buttonsContainer
+                }
+            }
+        }
+        .ignoresSafeArea()
+    }
+}
+
+// MARK: - UI Subviews
+
+private extension EditProfileView {
+
+    var backgroundLineGradient: some View {
+        LinearGradient(
+            gradient: Gradient(
+                colors: [
+                    .editProfLightGrad,
+                    .editProfDarkGrad
+                ]
+            ),
+            startPoint: .top,
+            endPoint: .center
+        )
+    }
+
+    var formsContainer: some View {
+        VStack(spacing: 0) {
+            Text(Constants.formsContainerTitle)
+                .foregroundStyle(Color.editProfWhite)
+                .font(Font.custom("Roboto", size: 20))
+                .padding(.bottom, 20)
+
+            NGGTextField(title: Constants.nameTextFieldPlaceholder, text: $inputName)
+            NGGTextField(title: Constants.surnameTextFieldPlaceholder, text: $inputSurname)
+            NGGTextField(title: Constants.emailTextFieldPlaceholder, text: $inputEmail)
+        }
+        .padding(.bottom, 124)
+    }
+
+    var avatarView: some View {
+        RoundedRectangle(cornerRadius: 30)
+            .fill(Color.editProfGray)
+            .frame(width: 150, height: 150)
+            .overlay(alignment: .bottomTrailing) {
+                Button {
+                    // TODO: IOS-12: Добавить логику обработки нажатий
+                    print("[DEBUG]: Добавить аватар")
+                } label: {
+                    Image(systemName: "plus.circle")
+                        .resizable()
+                        .foregroundStyle(.editProfWhite)
+                        .frame(width: 24, height: 24)
+                }
+            }
+    }
+
+    var headerView: some View {
+        ZStack(alignment: .top) {
+            UnevenRoundedRectangle(
+                cornerRadii: .init(
+                    bottomLeading: 24,
+                    bottomTrailing: 24
+                )
+            )
+            .fill(Color.editProfPurple)
+            .frame(height: 166)
+
+            Text(Constants.headerTitle)
+                .foregroundStyle(.editProfWhite)
+                .font(Font.custom("Roboto", size: 20))
+                .offset(y:55)
+                .padding(.horizontal, 16)
+
+            avatarView
+                .offset(y: 123)
+        }
+    }
+
+    var buttonsContainer: some View {
+        VStack(spacing: 0) {
+            NGGButton(Constants.saveButtonTitle) {
+                // TODO: IOS-12: Добавить логику обработки нажатий
+                print("[DEBUG]: Нажали кнопку сохранить и продолжить")
+            }
+            .padding(.bottom, 8)
+
+            Button {
+                // TODO: IOS-12: Добавить логику обработки нажатий
+                print("[DEBUG]: Сделать позже")
+            } label: {
+                Text(Constants.makeLaterButtonTitle)
+                    .underline()
+                    .foregroundColor(.editProfPurple)
+            }
+        }
+        .padding(.bottom, 64)
+    }
+}
+
+// MARK: - Preview
+
+#Preview {
+    EditProfileView()
+}
+
+// MARK: - Constants
+
+private extension EditProfileView {
+
+    enum Constants {
+        static let headerTitle = "Профиль"
+        static let formsContainerTitle = "Добавьте имя и фамилию"
+        static let nameTextFieldPlaceholder = "Имя"
+        static let surnameTextFieldPlaceholder = "Фамилия"
+        static let emailTextFieldPlaceholder = "pochta@pochta.com"
+        static let makeLaterButtonTitle = "Сделать позже"
+        static let saveButtonTitle = "Сохранить и продолжить"
+    }
+}
