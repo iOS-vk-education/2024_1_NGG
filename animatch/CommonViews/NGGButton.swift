4624a252263893de8d57be061d407f02f5396078
sechensy <90211409+sechensy@users.noreply.github.com>
2024-11-23 00:11:23 +0300

    Убрала offset + рефакторинг кода

---
+++ animatch/CommonViews/NGGButton.swift
@@ -0,0 +1,46 @@
+//
+//  NGGButton.swift
+//  animatch
+//
+//  Created by Ксения Панкратова on 18.11.2024.
+//
+
+import SwiftUI
+
+struct NGGButton: View {
+    private let title: String
+    private var action: (() -> Void)?
+
+    init(_ title: String, action: (() -> Void)? = nil) {
+        self.title = title
+        self.action = action
+    }
+
+    var body: some View {
+        Button(title) {
+            action?()
+        }
+        .buttonStyle(PurpleButtonStyle())
+    }
+}
+
+// MARK: - PurpleButtonStyle
+
+private struct PurpleButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .frame(width: 272, height: 51)
+            .background(Color.editProfPurple)
+            .foregroundStyle(Color.editProfWhite)
+            .clipShape(RoundedRectangle(cornerRadius: 8))
+    }
+}
+
+// MARK: - Preview
+
+#Preview {
+    NGGButton("Placeholder") {
+        print("[DEBUG]: Нажали на кнопку")
+    }
+}
